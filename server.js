840371a72c56a988b538b7444b417fe1184edecf
Steve Long <steve@gridtop.com>
2019-07-02 14:47:55 -0500

    setting up server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-// Set up a static folder (public) for our web app
-app.use(express.static("public"));
-
 // Initialize Express
 const app = express();
+
+// Set up a static folder (public) for our web app
+app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"; // change this
@@ -18,4 +18,9 @@ mongoose.connect(MONGODB_URI);
 // Main route (simple Hello World Message)
 app.get("/", function(req, res) {
   res.send("Hello world");
+});
+
+// Set the app to listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });

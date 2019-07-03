e0ee513194b7391577cf870aa0fba63b7d6f41d6
Steve Long <steve@gridtop.com>
2019-07-02 19:34:09 -0500

    slight change

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,7 +16,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-	console.log("We're connected.");
+	console.log("Connected to MongoDB.");
   // we're connected!
 });
 	

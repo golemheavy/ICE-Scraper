30726a30fc63784b5842fcdcb300f9a741418973
Steve Long <steve@gridtop.com>
2019-07-09 11:02:13 -0500

    changing the invocation of app.listen because it is hardcoding the port value

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// Set the app to listen on port 3000
+/*
 app.listen(3000, function() {
   console.log("App running on port 3000!");
+});
+*/
+
+
+// Set the app to listen on port 3000 or dynamic port set by environment variable
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
